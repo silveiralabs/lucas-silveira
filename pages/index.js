02940d9a02155c752995b95ebd682999de46ea50
Lucas Silveira <lsilveiralabs@gmail.com>
2022-07-05 23:17:38 -0300

    testando link

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 function Home() {
     return (<div>
         <h1>Home</h1>
-        <a href="/sobre">Acessar página Sobre</a>
+        
+        <Link href="/sobre">
+        <a>Acessar página Sobre</a>
+        </Link>
     </div>)
 }
 
--- pages/sobre.js
+++ pages/sobre.js
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 function Sobre() {
     return (<div>
         <h1>Sobre</h1>
-        <a href="/">Acessar página Home</a>
+        
+        <Link href="/">
+        <a>Acessar página Home</a>
+        </Link>
     </div>)
 }
 
